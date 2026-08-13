3f70b26dc0bffce331e166338ba5ce743fce69fc
Leandro CHOQUE <leandro.cristine1@gmail.com>
2026-08-13 11:32:43 -0300

    feat: modela estrutura da passagem do Brisamar

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,26 @@
+from app.models.passagem import (
+    EquipeMembro,
+    LadoLinha,
+    Linha,
+    PassagemBrisamarDetalhe,
+    PassagemLinhaOcupacao,
+    PassagemRadioUso,
+    PassagemServico,
+    Radio,
+    Terminal,
+)
+from app.models.usuario import Usuario
+
+
+__all__ = [
+    "EquipeMembro",
+    "LadoLinha",
+    "Linha",
+    "PassagemBrisamarDetalhe",
+    "PassagemLinhaOcupacao",
+    "PassagemRadioUso",
+    "PassagemServico",
+    "Radio",
+    "Terminal",
+    "Usuario",
+]
